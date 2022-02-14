14cf00c49c3fe6d034a0bb2afb73a466ffd28709
cbuxton1984 <cbuxton1984@gmail.com>
2022-02-14 17:40:44 +0000

    New files

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+location = "uksouth"
+rgname = "cb-pipelinedemo"
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "location" {
+  type = string
+  description = "Region for deployment"
+}
+
+variable "rgname" {
+    type = string
+    description = "Resource group name"
+}
+
